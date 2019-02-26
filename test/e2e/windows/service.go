d3040f19f9e06fe2c7a76cc6ce872174bb6e65bd
Muzz Imam <muimam@microsoft.com>
2019-02-26 11:34:19 -0800

    created new test

---
+++ test/e2e/windows/service.go
@@ -0,0 +1,121 @@
+/*
+Copyright 2016 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package windows
+
+import (
+	"fmt"
+
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
+	clientset "k8s.io/client-go/kubernetes"
+	"k8s.io/kubernetes/pkg/client/clientset_generated/internalclientset"
+	"k8s.io/kubernetes/test/e2e/framework"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+const (
+	defaultServeHostnameServicePort = 80
+	defaultServeHostnameServiceName = "svc-hostname"
+)
+
+var (
+	defaultServeHostnameService = v1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: defaultServeHostnameServiceName,
+		},
+		Spec: v1.ServiceSpec{
+			Ports: []v1.ServicePort{{
+				Port:       int32(defaultServeHostnameServicePort),
+				TargetPort: intstr.FromInt(9376),
+				Protocol:   v1.ProtocolTCP,
+			}},
+			Selector: map[string]string{
+				"name": defaultServeHostnameServiceName,
+			},
+		},
+	}
+)
+
+func getServeHostnameService(name string) *v1.Service {
+	svc := defaultServeHostnameService.DeepCopy()
+	svc.ObjectMeta.Name = name
+	svc.Spec.Selector["name"] = name
+	return svc
+}
+
+var _ = SIGDescribe("Services", func() {
+	f := framework.NewDefaultFramework("services")
+
+	var cs clientset.Interface
+	var internalClientset internalclientset.Interface
+	serviceLBNames := []string{}
+
+	BeforeEach(func() {
+		cs = f.ClientSet
+		internalClientset = f.InternalClientset
+	})
+
+	AfterEach(func() {
+		if CurrentGinkgoTestDescription().Failed {
+			framework.DescribeSvc(f.Namespace.Name)
+		}
+		for _, lb := range serviceLBNames {
+			framework.Logf("cleaning load balancer resource for %s", lb)
+			framework.CleanupServiceResources(cs, lb, framework.TestContext.CloudConfig.Region, framework.TestContext.CloudConfig.Zone)
+		}
+		//reset serviceLBNames
+		serviceLBNames = []string{}
+	})
+
+	// TODO: Run this test against the userspace proxy and nodes
+	// configured with a default deny firewall to validate that the
+	// proxy whitelists NodePort traffic.
+	It("should be able to create a functioning NodePort service", func() {
+		serviceName := "nodeport-test"
+		ns := f.Namespace.Name
+
+		jig := framework.NewServiceTestJig(cs, serviceName)
+		nodeIP := framework.PickNodeIP(jig.Client) // for later
+
+		By("creating service " + serviceName + " with type=NodePort in namespace " + ns)
+		service := jig.CreateTCPServiceOrFail(ns, func(svc *v1.Service) {
+			svc.Spec.Type = v1.ServiceTypeNodePort
+		})
+		jig.SanityCheckService(service, v1.ServiceTypeNodePort)
+		nodePort := int(service.Spec.Ports[0].NodePort)
+
+		By("creating pod to be part of service " + serviceName)
+		jig.RunOrFail(ns, nil)
+
+		By("hitting the pod through the service's NodePort")
+		jig.TestReachableHTTP(nodeIP, nodePort, framework.KubeProxyLagTimeout)
+
+		By("verifying the node port is locked")
+		hostExec := framework.LaunchHostExecPod(f.ClientSet, f.Namespace.Name, "hostexec")
+		// Even if the node-ip:node-port check above passed, this hostexec pod
+		// might fall on a node with a laggy kube-proxy.
+		cmd := fmt.Sprintf(`for i in $(seq 1 300); do if ss -ant46 'sport = :%d' | grep ^LISTEN; then exit 0; fi; sleep 1; done; exit 1`, nodePort)
+		stdout, err := framework.RunHostCmd(hostExec.Namespace, hostExec.Name, cmd)
+		if err != nil {
+			framework.Failf("expected node port %d to be in use, stdout: %v. err: %v", nodePort, stdout, err)
+		}
+	})
+
+})
